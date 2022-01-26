0c6c5f129ebde105d6ab31d9133c4bf6a9e8be76
ar_dehghan_a <ar.dehghan.a@gmail.com>
2022-01-26 20:14:30 +0330

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,25 @@
+const key = 'QDA46WPBiGkE7LWmuEau8zGJ9V6jNeaf';
+
+// Get weather information
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// Get city information
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
